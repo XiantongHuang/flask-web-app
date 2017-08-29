9e296e4ce3827c87496af69cf075f7c6942f7d0c
xthuang <xthuang1207@outlook.com>
2017-08-29 07:27:33 -0700

    使用jinjia2模板

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@
 __author__ = 'xthuang'
 
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager#输出一个Manager类，使支持命令行选项。
 
 
@@ -15,11 +15,13 @@ manager = Manager(app)#把程序实例作为参数传给构造函数，初始化
 
 @app.route('/')#注册视图函数 根目录
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+    #render_templates函数把jinjia2模板集成到程序中，第一个参数是模板的文件名随后     的参数都是键值对（默认在templates文件夹中寻找模板）
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
+    #左边的name表示参数名，就是模板中的占位符；右边的name是当前作用域中的变量，      表示同名参数值
 
 
 if __name__ == '__main__':
